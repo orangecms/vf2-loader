86deeabb0afbea458e623be2e30652ec8ec25e59
Daniel Maslowski <info@orangecms.org>
2023-03-02 22:30:08 +0100

    parse args with clap, list ports if not found

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
+use clap::Parser;
 use log::info;
 use std::fs::{metadata, File};
 use std::io::{stdout, Write};
 use xmodem::{Xmodem, XmodemCfg};
 
-fn main() {
-    env_logger::init();
-    info!("Sending fw.bin ...");
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Serial port
+    #[arg(short, long, default_value = "/dev/ttyUSB0")]
+    dev: String,
 
-    let ports = serialport::available_ports().expect("No serial ports found!");
-    for p in ports {
-        println!("{}", p.port_name);
-    }
-    // TODO: Do not hardcode this
-    let dev = "/dev/ttyUSB0";
-    // TODO: Do not hardcode this
-    let file = "./fw.bin";
-    let size = metadata(file).unwrap().len();
+    /// Firmware file
+    #[arg(index = 1)]
+    file: String,
+}
+fn main() {
+    let Args { dev, file } = Args::parse();
+
+    env_logger::init();
+    info!("Sending {file}...");
+
+    let size = metadata(file.clone()).unwrap().len();
     let mut file = File::open(file).unwrap();
 
-    let mut port = serialport::new(dev, 115_200)
-        .timeout(std::time::Duration::from_secs(5))
+    let mut port = match serialport::new(dev.clone(), 115_200)
+        .timeout(std::time::Duration::from_secs(1))
         .open()
-        .expect("Failed to open port");
+    {
+        Ok(d) => d,
+        Err(_) => {
+            println!("Failed to open serial port {dev}; available ports:");
+            let ports = serialport::available_ports().expect("No serial ports found!");
+
+            for p in ports {
+                println!("{}", p.port_name);
+            }
+            std::process::exit(1);
+        }
+    };
 
     const PACKET_SIZE: u32 = 128;
     let progress = |p: u32| {
@@ -34,7 +51,7 @@ fn main() {
         }
         stdout().flush().ok();
     };
-    let buf: &mut [u8; 1] = &mut [0 as u8];
+    let buf: &mut [u8; 1] = &mut [0];
     while buf[0] != b'C' {
         port.read_exact(buf).ok();
     }
